3ca53f861bf7e9a51fd0bf4f920ec02b626c2f5b
Vidhyasagar <vidhyasagarmyana9598@gmail.com>
2025-05-21 18:54:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let name = "Vidhyasagar";
+let salary = 2500000;
+let skills = ["JavaScript", "AI", "React"];
+
+console.log(`My name is ${name}`);
+console.log(`I want a ₹${salary / 100000} LPA job`);
+console.log("Skills I will learn:", skills);
