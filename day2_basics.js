60274c1bf55f199c4666ba7cbd78189fa4d81fc9
Vidhyasagar <95684474+1Vidhyasagar@users.noreply.github.com>
2025-05-26 22:17:30 +0530

    Update day2_basics.js

--- day2_basics.js
+++ day2_basics.js
@@ -5,8 +5,11 @@ let isHungry = true;
 console.log("Hi, my name is " + name);
 console.log("Am I hungry? " + isHungry);
 
+// Checking if age is 18 or above
 if (age >= 18) {
   console.log("✅ You can vote!");
 } else {
   console.log("❌ Sorry, you can't vote.");
 }
+
+console.log(typeof age); // number
